c0275c85ce0e65c755207e7dfdfaa5c824a1dfff
cou <davidestradag200@gmail.com>
2023-02-02 14:28:35 -0500

    juegodiesel

---
+++ src/App.jsx
@@ -0,0 +1,157 @@
+import preguntasDieselTrivia from "./preguntas"; 
+import { useState, useEffect } from "react";
+
+function App() {
+  const [preguntaActual, setPreguntaActual] = useState(0);
+  const [puntuación, setPuntuación] = useState(0);
+  const [isFinished, setIsFinished] = useState(false);
+  const [tiempoRestante, setTiempoRestante] = useState(40);
+  const [areDisabled, setAreDisabled] = useState(false);
+  const [answersShown, setAnswersShown] = useState(false);
+
+
+
+
+  function handleAnswerSubmit(isCorrect, e) {
+    // añadir puntuación
+    if (isCorrect) setPuntuación(puntuación + 1);
+    // añadir estilos de pregunta
+    e.target.classList.add(isCorrect ? "correct" : "incorrect");
+    // cambiar a la siguiente pregunta
+
+    setTimeout(() => {
+      if (preguntaActual === preguntasDieselTrivia.length - 1) {
+        setIsFinished(true);
+      } else {
+        setPreguntaActual(preguntaActual + 1);
+        setTiempoRestante(40);
+      }
+    }, 1500);
+  }
+
+  useEffect(() => {
+    const intervalo = setInterval(() => {
+      if (tiempoRestante > 0) setTiempoRestante((prev) => prev - 1);
+      if (tiempoRestante === 0) setAreDisabled(true);
+    }, 1000);
+
+    return () => clearInterval(intervalo);
+  }, [tiempoRestante]);
+  // const puntuacionCorrecto = function(){
+  //   if(puntuacionCorrecto <= 3){
+  //     return console.log("Correcto")
+  //   }else if (puntuacionCorrecto >= 3){
+  //       return console.log("Incorrecto")
+      
+  //   }
+  //  }
+
+
+  if (isFinished){
+  return (
+      <main className="app">
+        <div className="juego-terminado">
+          <span>
+            {" "}
+            Obtuviste {puntuación} de {preguntasDieselTrivia.length}{" "}            
+          </span><h4>Cupon:diesel</h4>
+          <button onClick={() => (window.location.href = "/")}>
+            {" "}
+            Volver a jugar
+          </button>
+          <button
+            onClick={() => {
+              setIsFinished(false);
+              setAnswersShown(true);
+              setPreguntaActual(0);
+            }}
+          >
+            Ver respuestas
+          </button>
+        </div>
+      </main>
+    );
+  }
+  
+
+  if (answersShown)
+    return (
+      <main className="app">
+        <div className="lado-izquierdo">
+          <div className="numero-pregunta">
+            <span> Pregunta {preguntaActual + 1} de</span> {preguntasDieselTrivia.length}
+          </div>
+          <div className="titulo-pregunta">
+            {preguntasDieselTrivia[preguntaActual].titulo}
+          </div>
+          <div>
+            {
+              preguntasDieselTrivia[preguntaActual].opciones.filter(
+                (opcion) => opcion.isCorrect
+              )[0].textoRespuesta
+            }
+          </div>
+          <button
+            onClick={() => {
+              if (preguntaActual === preguntasDieselTrivia.length - 1) {
+                window.location.href = "/";
+              } else {
+                setPreguntaActual(preguntaActual + 1);
+              }
+            }}
+          >
+            {preguntaActual === preguntasDieselTrivia.length - 1
+              ? "Volver a jugar"
+              : "Siguiente"}
+          </button>
+        </div>
+      </main>
+    );
+
+  return (
+    <main className="app">
+      <div className="lado-izquierdo">
+        <div className="numero-pregunta">
+          <span> Pregunta {preguntaActual + 1} de</span> {preguntasDieselTrivia.length}
+        </div>
+        <div className="titulo-pregunta">
+          {preguntasDieselTrivia[preguntaActual].titulo}
+        </div>
+        <div>
+          {!areDisabled ? (
+            <span className="tiempo-restante">
+              Tiempo restante: {tiempoRestante}{" "}
+            </span>
+          ) : (
+            <button
+              onClick={() => {
+                setTiempoRestante(10);
+                setAreDisabled(false);
+                if (preguntaActual === preguntasDieselTrivia.length - 1) {
+                  setIsFinished(true);
+                } else {
+                  setPreguntaActual(preguntaActual + 1);
+                }
+              }}
+            >
+              Continuar
+            </button>
+          )}
+        </div>
+      </div>
+      <div className="lado-derecho">
+        {preguntasDieselTrivia[preguntaActual].opciones.map((respuesta) => (
+          <button
+            disabled={areDisabled}
+            key={respuesta.textoRespuesta}
+            onClick={(e) => handleAnswerSubmit(respuesta.isCorrect, e)}
+          >
+            {respuesta.textoRespuesta}
+          </button>
+        ))}
+      </div>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/preguntas.js
@@ -0,0 +1,49 @@
+const preguntasDieselTrivia = [
+    {
+        titulo:"¿Cuando comienza stereo picnic?",
+        opciones:[
+        {textoRespuesta: "23 de Marzo", isCorrect: true},
+        {textoRespuesta: "25 de Marzo", isCorrect: false},
+        {textoRespuesta: "20 de Abril", isCorrect: false},
+        {textoRespuesta: "19 de Marzo", isCorrect: false},
+        ],
+    },
+    {
+        titulo:"¿En que ciudad se hara estero picnic?",
+        opciones:[
+            {textoRespuesta:"Medellin", isCorrect: false},
+            {textoRespuesta:"Cali", isCorrect: false},
+            {textoRespuesta:"Bogota", isCorrect: true},
+            {textoRespuesta:"Santa Marta", isCorrect: false},
+        ],
+    },
+    {
+        titulo:"¿Que genero canta Drake?",
+        opciones:[
+            {textoRespuesta:"Reggue", isCorrect: false},
+            {textoRespuesta:"Pop", isCorrect: false},
+            {textoRespuesta:"Rock", isCorrect: false},
+            {textoRespuesta:"Rap", isCorrect: true},
+        ],
+    },
+    {
+        titulo:"¿Cual de estos artistas es de colombia?",
+        opciones:[
+            {textoRespuesta:"Bizzarap", isCorrect: false},
+            {textoRespuesta:"Trueno", isCorrect: false},
+            {textoRespuesta:"Kali Uchis", isCorrect: true},
+            {textoRespuesta:"jerry rivera", isCorrect: false},
+        ],
+    },
+    {
+        titulo:"¿Cual de estos es patrocinador de estero picnic?",
+        opciones:[
+           {textoRespuesta:"Diesel", isCorrect: true},
+           {textoRespuesta:"Pilatos", isCorrect: false},
+           {textoRespuesta:"Kipling", isCorrect: false},
+           {textoRespuesta:"SuperDry", isCorrect: false},
+        ]
+    }
+]
+
+export default preguntasDieselTrivia
